8dba8af4a696cfc3675446a3ff1b6c12c550db02
Foohooboo <danielmarshallevans@gmail.com>
2019-02-27 11:48:03 -0700

    error is an ack, ack message requires reference ID

--- Project/Shared/Shared/comms/messages/AckMessage.cs
+++ Project/Shared/Shared/comms/messages/AckMessage.cs
@@ -1,15 +1,20 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-
-namespace Shared.Comms.Messages
+﻿namespace Shared.Comms.Messages
 {
     public class AckMessage : Message
     {
-        public AckMessage() : base()
+        public AckMessage(int ReferenceMessageID) : base()
         {
-
+            this.ReferenceMessageID = ReferenceMessageID;
         }
 
-        public string AckHello;
+        public int ReferenceMessageID
+        {
+            get; set;
+        }
+
+        public string AckHello
+        {
+            get; set;
+        }
     }
 }
