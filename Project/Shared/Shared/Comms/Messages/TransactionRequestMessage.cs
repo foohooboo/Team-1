248edf10a9b721a024e539c3bd97eb4a335e1c11
Foohooboo <danielmarshallevans@gmail.com>
2019-02-27 18:58:36 -0700

    Implemented TransactionRequestMessage

---
+++ Project/Shared/Shared/Comms/Messages/TransactionRequestMessage.cs
@@ -0,0 +1,21 @@
+﻿namespace Shared.Comms.Messages
+{
+    public class TransactionRequestMessage : Message
+    {
+        public TransactionRequestMessage()
+        {
+
+        }
+
+        //Positive value for buy, negative value for sell
+        public int Quantity
+        {
+            get; set;
+        }
+
+        public Shared.EvaluatedStock StockValue
+        {
+            get; set;
+        }
+    }
+}
