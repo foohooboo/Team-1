95fd298b6adc864950896366ad6f43ed4b56ebf6
Foohooboo <danielmarshallevans@gmail.com>
2019-02-27 22:41:03 -0700

    Basic tests for Ack/Error messages, expanded AckSerializTest slightly

---
+++ Project/Shared/SharedTest/Messages/AckTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shared.Comms.Messages;
+
+namespace SharedTest.Messages
+{
+    [TestClass]
+    public class AckTest
+    {
+        [TestMethod]
+        public void DefaultConstructorTest()
+        {
+            var newAck = new AckMessage();
+
+            Assert.IsNull(newAck.ReferenceMessageID);
+            Assert.IsNull(newAck.AckHello);
+        }
+
+        [TestMethod]
+        public void InitializeTest()
+        {
+            string referenceID = "1-2-3";
+            string hello = "hello";
+
+            var m = new AckMessage
+            {
+                ReferenceMessageID = referenceID,
+                AckHello = hello
+            };
+
+            Assert.AreEqual(referenceID, m.ReferenceMessageID);
+            Assert.AreEqual(hello, m.AckHello);
+        }
+
+        [TestMethod]
+        public void SerilaizeTest()
+        {
+            string referenceID = "1-2-3";
+            string hello = "hello";
+
+            var m = new AckMessage
+            {
+                ReferenceMessageID = referenceID,
+                AckHello = hello
+            };
+
+            var serailizedMessage = MessageFactory.GetMessage(m.Encode()) as AckMessage;
+
+            Assert.AreEqual(serailizedMessage.ReferenceMessageID, m.ReferenceMessageID);
+            Assert.AreEqual(serailizedMessage.AckHello, m.AckHello);
+        }
+    }
+}
---
+++ Project/Shared/SharedTest/Messages/ErrorTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shared.Comms.Messages;
+
+namespace SharedTest.Messages
+{
+    [TestClass]
+    public class ErrorTest
+    {
+        [TestMethod]
+        public void DefaultConstructorTest()
+        {
+            var newError = new ErrorMessage();
+
+            Assert.IsNull(newError.ErrorText);
+        }
+
+        [TestMethod]
+        public void InitializeTest()
+        {
+            string err = "error";
+
+            var m = new ErrorMessage
+            {
+                ErrorText = err
+            };
+
+            Assert.AreEqual(err, m.ErrorText);
+        }
+
+        [TestMethod]
+        public void SerializeTest()
+        {
+            string err = "error";
+
+            var m = new ErrorMessage
+            {
+                ErrorText = err
+            };
+
+            var serailizedMessage = MessageFactory.GetMessage(m.Encode()) as ErrorMessage;
+
+            Assert.AreEqual(m.ErrorText, serailizedMessage.ErrorText);
+        }
+    }
+}
