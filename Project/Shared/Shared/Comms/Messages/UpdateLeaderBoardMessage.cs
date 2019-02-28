7c160881237229c1418cf90ee015f024d34427a2
Foohooboo <danielmarshallevans@gmail.com>
2019-02-27 17:53:47 -0700

    implemented UpdateLeaderBoardMessage (inherits Message, not GetPortfolioRequest)

---
+++ Project/Shared/Shared/Comms/Messages/UpdateLeaderBoardMessage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Shared.Comms.Messages
+{
+    class UpdateLeaderBoardMessage : Message
+    {
+        public UpdateLeaderBoardMessage()
+        {
+
+        }
+
+        public Dictionary<string, double> Records
+        {
+            get; set;
+        }
+    }
+}
