4a270ca4e5865b69e190bef8c1c5dd180fef84ba
Foohooboo <danielmarshallevans@gmail.com>
2019-02-28 19:38:41 -0700

    Basic CreatePortfolioRequestMessage tests

---
+++ Project/Shared/SharedTest/Messages/CreatePortfolioRequestMessageTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shared.Comms.Messages;
+
+namespace SharedTest.Messages
+{
+    [TestClass]
+    public class CreatePortfolioRequestMessageTest
+    {
+        public CreatePortfolioRequestMessageTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void DefaultConstructorTest()
+        {
+            var newCreatePortfolioRequest = new CreatePortfolioRequestMessage();
+
+            Assert.IsNull(newCreatePortfolioRequest.ConfirmPassword);
+        }
+
+        [TestMethod]
+        public void InheritsGetPortfolioRequestTest()
+        {
+            var newCreatePortfolioRequest = new CreatePortfolioRequestMessage();
+
+            Assert.IsNull(newCreatePortfolioRequest.Account);
+        }
+
+        [TestMethod]
+        public void TestInitialize()
+        {
+            string password = "yolo";
+
+            var newCreatePortfolioRequest = new CreatePortfolioRequestMessage
+            {
+                ConfirmPassword = password
+            };
+        }
+    }
+}
