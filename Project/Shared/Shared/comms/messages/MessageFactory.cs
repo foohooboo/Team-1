227e550a48464b2aa2a737d03f81b1a5780dc44e
dsphar <david.sphar@aggiemail.usu.edu>
2019-03-03 20:50:18 -0700

    Hack incoming JSON in prep for decoding. Is there a Newtonsoft setting that would fix this without a hack?

--- Project/Shared/Shared/comms/messages/MessageFactory.cs
+++ Project/Shared/Shared/comms/messages/MessageFactory.cs
@@ -9,7 +9,11 @@ namespace Shared.Comms.Messages
     {
         private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
-        public static readonly JsonSerializerSettings settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All };
+        public static readonly JsonSerializerSettings settings = new JsonSerializerSettings {
+            TypeNameHandling = TypeNameHandling.All
+        };
+
+        private static readonly string ProjectName = System.Reflection.Assembly.GetEntryAssembly().GetName().Name;
 
         public static int MessageCount
         {
@@ -19,13 +23,22 @@ namespace Shared.Comms.Messages
         public static Message GetMessage(byte[] bytes)
         {
             string readData = Encoding.Default.GetString(bytes);
-
             return (GetMessage(readData));
         }
 
         public static Message GetMessage(string json)
         {
             Log.Debug($"{nameof(GetMessage)} (enter)");
+
+            //Note: The following hack was added because we are using the Newtonsoft serializer on different projects.
+            //It adds the project name to the Type field in JSON, and this causes an exception to be thrown when
+            //decoding json from projects with a different name. We might be able to replace this hack with a setting
+            //that allows cross-project serialization, I just couldn't find that setting after a 20 minute Google-fu
+            //session. Hence I decided to hack this before the assignment is due.
+            //-dsphar 3/3/2019
+            int startHackIndex = json.IndexOf(',') + 1;
+            int endHackIndex = json.Substring(startHackIndex).IndexOf('"');
+            var hackedjson = $"{json.Substring(0, startHackIndex)} {ProjectName}{json.Substring(endHackIndex + startHackIndex)}";
 
             var v = JsonConvert.DeserializeObject<Message>(json, settings);
 
