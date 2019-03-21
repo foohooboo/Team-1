25c71d62830525dd91fa1ff36d19377649537e73
dsphar <david.sphar@aggiemail.usu.edu>
2019-03-20 22:51:54 -0600

    StockData rework...
    Removed magic filenames: Now loads any csv found in HistoricData directory.
    
    Dates are now synced: To better simulate a unified market, the stock-data loader now skips days which are missing in one or more files. (now a market-wide crash in the data will be reflected in this simulation)
    
    Note: I kept the random scaler and the fake date obfuscation. It is unlikely anyone can figure out the real dates while playing.

--- Project/StockServer/StockServer/Data/StockData.cs
+++ Project/StockServer/StockServer/Data/StockData.cs
@@ -2,6 +2,7 @@
 using Shared.MarketStructures;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace StockServer.Data
 {
@@ -21,6 +22,18 @@ namespace StockServer.Data
             set { }
         }
 
+        public static void Init()
+        {
+            Log.Debug($"{nameof(Init)} (enter)");
+
+            if (_fullHistory == null)
+                LoadStocksFromFile();
+
+            CurrentDayNumber = 0;
+
+            Log.Debug($"{nameof(Init)} (exit)");
+        }
+
         private static int _currentDayNumber;
         public static int CurrentDayNumber
         {
@@ -37,18 +50,6 @@ namespace StockServer.Data
             }
         }
 
-        public static void Init()
-        {
-            Log.Debug($"{nameof(Init)} (enter)");
-
-            if (_fullHistory == null)
-                _fullHistory = LoadStocksFromFile();
-
-            CurrentDayNumber = 0;
-
-            Log.Debug($"{nameof(Init)} (exit)");
-        }
-
         public static int GetSize()
         {
             return FullHistory.Count;
@@ -58,12 +59,12 @@ namespace StockServer.Data
         {
             int currentIndex = CurrentDayNumber - numDays; ;
             if (currentIndex < 0)
-                currentIndex = (currentIndex + GetSize()+1) % GetSize();//account for rollover
+                currentIndex = (currentIndex + GetSize() + 1) % GetSize();//account for rollover
 
             var segment = new MarketSegment();
-            for(int i=0; i<numDays; i++)
+            for (int i = 0; i < numDays; i++)
             {
-                
+
                 segment.Add(FullHistory[currentIndex]);
                 currentIndex = (++currentIndex) % GetSize();
             }
@@ -82,91 +83,147 @@ namespace StockServer.Data
             return GetCurrentDay();
         }
 
-        private static MarketSegment LoadStocksFromFile()
+        private static DateTime GetDate(string dayLine)
         {
-            /* To Add a new Stock: use this link and replace [SYMBOL] with a company's symbol
-             * http://download.macrotrends.net/assets/php/stock_data_export.php?t=[SYMBOL]
-             * Rename file to [SYMBOL].csv
-             * Put file in StockServer/bin/HistoricData
-             * Add tuple to hist[] below
-            */
+            return (DateTime.Parse(dayLine.Substring(0, dayLine.IndexOf(','))));
+        }
 
-            //TODO: If we ever remove the Hist[,] below, we will probably also want to remove this magic "days" number.
-            //It might be a pain, but one way is to step every file and just count the number of days, finding the smallest
-            //and setting this value to that. That way the files can live completely on their own and users can change them
-            //to any size without having to re-compile. -Dsphar 3/1/2019
-            int days = 1000;//Should be shorter than the shortest stock history.
-
-
-            MarketSegment ret = new MarketSegment();//gets returned
-            DateTime date = DateTime.Now;
-            date = date.AddYears(50);
-            for (int i = 0; i < days; i++)
+        private static DateTime NextWeekday(DateTime start)
+        {
+            var day = start.AddDays(1);
+            while ((day.DayOfWeek == DayOfWeek.Saturday) || (day.DayOfWeek == DayOfWeek.Sunday))
             {
-                date = date.AddDays(1);
-                if ((date.DayOfWeek == DayOfWeek.Saturday) || (date.DayOfWeek == DayOfWeek.Sunday))
-                {
-                    date = date.AddDays(1);
-                }
-                ret.Add(new MarketDay(date.ToString("yyyy-MM-dd")));
+                day = day.AddDays(1);
             }
-            //TODO: It might be a good idea to have this loader try to parse every file in some directory.
-            //That way we wont require any "magic" filenames like hist[,] below. The csv files themselves
-            //could contain the company name and symbol. Not worth changing now, but maybe if we have time
-            //in the future.     -Dsphar 2/27/2019
+            return day;
+        }
 
-
-            string[,] hist = new string[,] {{"AAPL", "Apple Inc."}, {"AMZN", "Amazon.com Inc"}, {"BABA", "Alibaba Group"}
-                , {"BAC", "Bank of America Corp." }, {"BUD", "Anheuser-Busch Inbev"}, {"CVX", "Chevron Corperation"}, {"FB", "Facebook Inc."}
-                , {"GOOGL", "Alphabet"}, {"HD", "Home Depot Inc."}, {"JNJ", "Johnson & Johnson"}, {"JPM", "JPMorgan Chase & Co."}, { "MSFT", "Microsoft"}
-                , { "PFE", "Pfizer Inc."}, {"PG", "Procter & Gamble Co."}, { "T", "AT&T Inc."}, { "UNH", "UnitedHealth Group Inc."}, { "V", "Visa Inc."}
-                , {"WFC", "Wells Fargo & Co."}, { "WMT", "Walmart Inc."}, { "XOM", "ExxonMobil Corporation"}, { "GOOG", "Alphabet"}
-            };
-
-            List<Stock> stocks = new List<Stock>();
-
-            Random random = new Random();
-            int dates = random.Next(0, hist.GetLength(0));
-
-            for (int i = 0; i < hist.GetLength(0); i++)
+        private static void LoadStocksFromFile()
+        {
+            if (_fullHistory != null)
             {
+                throw new Exception("Cannot LoadStocksFromFile more than once.");
+            }
 
-                stocks.Add(new Stock(hist[i, 0], hist[i, 1]));
-                List<ValuatedStock> SingleStockUpdates = new List<ValuatedStock>();
-                float mult = (float)Math.Pow(2, (random.NextDouble() * 4 - 2));//gives nice range for multiplier
-                using (var reader = new System.IO.StreamReader("HistoricData/" + hist[i, 0] + ".csv"))//open file based on symbol
+            _fullHistory = new MarketSegment();
+            var random = new Random();
+            int range = 15 * 365;
+            DateTime coverDate = DateTime.Today.AddDays(random.Next(range));
+            coverDate = NextWeekday(coverDate);
+
+            //Import all .csv files into companiesRaw
+            var companiesRaw = new List<CompanyDataRaw>();
+            foreach (string filename in Directory.EnumerateFiles("HistoricData", "*.csv"))
+            {
+                using (var fileReader = new StreamReader(filename))
                 {
-                    for (int j = 0; j < 11; j++) { reader.ReadLine(); }//skip the first 11 lines
-                    //Read in a stock, for all time,
-                    //randomly pick n days in order
-                    //multiply by random constant
-                    //TODO: Randomize Name and Symbol
-                    //build up ret
-                    while (!reader.EndOfStream)
-                    {
-                        string line = reader.ReadLine();
-                        string[] values = line.Split(',');
-                        //Format: string[date,open,high,low,close,volume]
+                    var currentCompany = new CompanyDataRaw();
 
-                        SingleStockUpdates.Add(new ValuatedStock(values, stocks[i]));
-                    }
-                    int l = SingleStockUpdates.Count;
-                    int lr = random.Next(0, l - days);
-                    SingleStockUpdates.RemoveRange(0, lr);
-                    SingleStockUpdates.RemoveRange(days, SingleStockUpdates.Count - days);//use days long range and remove everything else
-                    for (int j = 0; j < days; j++)
+                    var symbol = fileReader.ReadLine().Trim('"');
+                    var name = fileReader.ReadLine().Trim('"');
+                    currentCompany.MetaData = new Stock(symbol, name);
+
+                    //Prepare filestream
+                    fileReader.ReadLine();
+                    fileReader.ReadLine();
+                    fileReader.ReadLine();
+                    while (!fileReader.EndOfStream)
                     {
-                        //multiply values by the same number for a whole stock history so its hard to cheat.
-                        SingleStockUpdates[j].Close = (float)Math.Round((double)(SingleStockUpdates[j].Close * mult), 2);
-                        SingleStockUpdates[j].Open = (float)Math.Round((double)(SingleStockUpdates[j].Open * mult), 2);
-                        SingleStockUpdates[j].High = (float)Math.Round((double)(SingleStockUpdates[j].High * mult), 2);
-                        SingleStockUpdates[j].Low = (float)Math.Round((double)(SingleStockUpdates[j].Low * mult), 2);
-                        SingleStockUpdates[j].Volume = (int)((float)SingleStockUpdates[j].Volume / mult);
-                        ret[j].TradedCompanies.Add(SingleStockUpdates[j]);
+                        currentCompany.DailyValue.Add(fileReader.ReadLine());
                     }
+                    companiesRaw.Add(currentCompany);
                 }
             }
-            return ret;
+
+            //Cache stock data (only when all files have data for the given day)
+            var keepParsing = true;
+            float mult = (float)Math.Pow(2, (random.NextDouble() * 4 - 2));//gives nice range for multiplier
+            for (int dayIndex = 0; dayIndex < companiesRaw[0].DailyValue.Count - 1 && keepParsing; dayIndex++)
+            {
+                DateTime currentDate = GetDate(companiesRaw[0].CurrentDayData());
+                foreach (var company in companiesRaw)
+                {
+                    if (!company.AdvanceTo(currentDate))
+                    {
+                        //company data ended before currentDate
+                        keepParsing = false;
+                        break;
+                    }
+                }
+
+                if (keepParsing)
+                {
+                    //See if all days are the same
+                    var isSynced = true;
+                    foreach (var company in companiesRaw)
+                    {
+                        if (!company.CurrentDate.Equals(currentDate))
+                        {
+                            isSynced = false;
+                            break;
+                        }
+                    }
+
+                    if (isSynced)
+                    {
+                        //all files are at the current date, add to history
+                        var marketDay = new MarketDay(coverDate.ToString());
+                        coverDate = NextWeekday(coverDate);
+                        foreach (var company in companiesRaw)
+                        {
+                            var vStock = new ValuatedStock(company.CurrentDayData().Split(','), company.MetaData);
+
+                            vStock.Close = (float)Math.Round((vStock.Close * mult), 2);
+                            vStock.Open = (float)Math.Round((vStock.Open * mult), 2);
+                            vStock.High = (float)Math.Round((vStock.High * mult), 2);
+                            vStock.Low = (float)Math.Round((vStock.Low * mult), 2);
+                            vStock.Volume = (int)(vStock.Volume / mult);
+
+                            marketDay.TradedCompanies.Add(vStock);
+                        }
+                        _fullHistory.Add(marketDay);
+                    }
+
+                    companiesRaw[0].AdvanceDay();
+                }
+            }
+        }
+
+        private sealed class CompanyDataRaw
+        {
+            public Stock MetaData;
+            public DateTime CurrentDate { get; private set; }
+            public List<string> DailyValue = new List<string>();
+
+            private int Index;
+
+            public string CurrentDayData()
+            {
+                return DailyValue[Index];
+            }
+
+            public string AdvanceDay()
+            {
+                ++Index;
+                CurrentDate = GetDate(DailyValue[Index]);
+                return DailyValue[Index];
+            }
+
+            public bool AdvanceTo(DateTime date)
+            {
+                var succeed = true;
+                try
+                {
+                    var currentDate = GetDate(CurrentDayData());
+                    while (currentDate < date)
+                        currentDate = GetDate(AdvanceDay());
+                }
+                catch
+                {
+                    succeed = false;
+                }
+                return succeed;
+            }
         }
 
         /// <summary>
