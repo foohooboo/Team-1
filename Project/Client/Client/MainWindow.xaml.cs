c655022137ed97a410207b2e47abb589aaf7a0f1
dsphar <david.sphar@aggiemail.usu.edu>
2019-03-31 16:03:22 -0600

    Decouple SendTransaction from dummy "mem" variable

--- Project/Client/Client/MainWindow.xaml.cs
+++ Project/Client/Client/MainWindow.xaml.cs
@@ -1,14 +1,11 @@
 ﻿using CommSystem;
 using log4net;
+using Shared.MarketStructures;
 using System;
 using System.ComponentModel;
 using System.Windows;
-using Shared.MarketStructures;
-using Shared.PortfolioResources;
-using System.Collections.Generic;
 using System.Windows.Controls;
 using System.Windows.Shapes;
-using System.Windows.Forms.VisualStyles;
 
 namespace Client
 {
@@ -22,10 +19,10 @@ namespace Client
         private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         private HelloWorld helloWorld = new HelloWorld();
         public event PropertyChangedEventHandler PropertyChanged;
-        
+
         public MainWindow()
         {
-            
+
             string method = "MainWindow Constructor";
             Log.Debug(string.Format("Enter - {0}", method));
 
@@ -36,8 +33,8 @@ namespace Client
             HelloTextLocal = helloWorld.HelloText;
 
             Log.Debug(string.Format("Exit - {0}", method));
-            
-            
+
+
         }
 
         private string helloTextLocal;
@@ -50,7 +47,7 @@ namespace Client
                 {
                     helloTextLocal = value;
                     helloWorld.HelloText = value;
-                    
+
                     //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("HelloTextLocal"));
                 }
             }
@@ -58,8 +55,8 @@ namespace Client
         public void updateStockPanels()
         {
             stockPanels.Items.Clear();
-            MarketDay day = mem.History[mem.History.Count-1];
-            foreach(ValuatedStock i in day.TradedCompanies)
+            MarketDay day = mem.History[mem.History.Count - 1];
+            foreach (ValuatedStock i in day.TradedCompanies)
             {
                 Canvas shell = new Canvas();
                 shell.Name = i.Symbol;
@@ -69,23 +66,23 @@ namespace Client
                 back.Height = 52;
                 back.Width = 183;
                 TextBlock sym = new TextBlock();
-                sym.Text =i.Symbol;
+                sym.Text = i.Symbol;
                 sym.Margin = new Thickness(2);
                 TextBlock val = new TextBlock();
                 val.Text = i.Close.ToString("0.00");
                 val.Width = 179;
                 val.TextAlignment = TextAlignment.Right;
-                
-                shell.Children.Insert(0,back);
+
+                shell.Children.Insert(0, back);
                 shell.Children.Insert(1, sym);
                 shell.Children.Insert(2, val);
-                
+
                 stockPanels.Items.Add(shell);
-                
+
             }
-                
+
         }
-        
+
         public void OnHelloTextChanged(object source, EventArgs args)
         {
             string method = "OnHelloTextChanged";
@@ -102,13 +99,22 @@ namespace Client
             //This function should check if the transaction is possible and if not will just do its best.
             //Instead of selling 100 it just sells what you have.
             //instead of buying 100 it just buys as much as your cash can afford.
-            HelloTextLocal = amount.ToString() + "of"+mem.SelectedAsset.RelatedStock.Name;
-            updateStockPanels();
+
+            var selectedStock = stockPanels.SelectedItem as Canvas;
+            if (selectedStock != null)
+            {
+                var symbol = (selectedStock.Children[1] as TextBlock).Text;
+                HelloTextLocal = $"Initiated transaction for {amount} shares of {symbol}";
+            }
+            else
+            {
+                Log.Warn("No stocks selected, cannot send transaction.");
+            }
         }
         private void SellOutEvent(object sender, RoutedEventArgs e)
         {
             SendTransaction(-mem.SelectedAsset.Quantity);
- 
+
         }
 
         private void Sell100Event(object sender, RoutedEventArgs e)
@@ -137,14 +143,16 @@ namespace Client
 
         private void BuyEvent(object sender, RoutedEventArgs e)
         {
+
+
             SendTransaction(Int32.Parse(StockCount));
-            
+
         }
 
         private void SellEvent(object sender, RoutedEventArgs e)
         {
             SendTransaction(-Int32.Parse(StockCount));
-           
+
         }
 
         private void ListBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
@@ -160,9 +168,9 @@ namespace Client
         private void Makeup_Click(object sender, RoutedEventArgs e)
         {
             mem.Cash = 100000;
-            mem.History = ManagedData.makeupMarketSegment(15,30);
+            mem.History = ManagedData.makeupMarketSegment(15, 30);
             mem.MyPortfolio = ManagedData.makeupPortfolio(mem.History[0]);
-            makeup.IsEnabled=false;
+            makeup.IsEnabled = false;
             updateStockPanels();
         }
     }
