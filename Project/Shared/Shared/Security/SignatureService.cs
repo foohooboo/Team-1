210e3609126c3a87d9893aeb116390ffc14522bd
dsphar <david.sphar@aggiemail.usu.edu>
2019-04-06 12:07:11 -0600

    Rough of sig object and verify sig object

--- Project/Shared/Shared/Security/SignatureService.cs
+++ Project/Shared/Shared/Security/SignatureService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization;
@@ -10,7 +11,61 @@ namespace Shared.Security
 {
     public class SignatureService
     {
+        private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
+        private static readonly HashAlgorithm HASH_ALGORITHM = new SHA1CryptoServiceProvider();
+
+        public string GetSignature<TSignedObject>(TSignedObject obj, RSAParameters privateKey)
+        {
+            Log.Debug($"{nameof(GetSignature)} (enter)");
+
+            string sig = "";
+
+            try
+            {
+                byte[] serlializedObj = Serialize(obj);
+
+                RSACryptoServiceProvider RSAalg = new RSACryptoServiceProvider();
+                RSAalg.ImportParameters(privateKey);
+
+                var sigBytes = RSAalg.SignData(serlializedObj, HASH_ALGORITHM);
+                sig = Convert.ToBase64String(sigBytes);
+            }
+            catch (Exception e)
+            {
+                Log.Error($"Problem signing {obj.GetType().Name} object...");
+                Log.Error(e);
+            }
+
+            Log.Debug($"{nameof(GetSignature)} (exit)");
+            return sig;
+        }
+
+        public bool VerifySignature<TSignedObject>(TSignedObject obj, string signature, RSAParameters publicKey)
+        {
+            Log.Debug($"{nameof(VerifySignature)} (enter)");
+
+            var isValid = false;
+
+            try
+            {
+                byte[] SignedData = Convert.FromBase64String(signature);
+                byte[] DataToVerify = Serialize(obj);
+
+                RSACryptoServiceProvider RSAalg = new RSACryptoServiceProvider();
+                RSAalg.ImportParameters(publicKey);
+
+                isValid = RSAalg.VerifyData(DataToVerify, new SHA1CryptoServiceProvider(), SignedData);
+            }
+            catch (Exception e)
+            {
+                Log.Error($"Problem verifying signature of {obj.GetType().Name} object...");
+                Log.Error(e);
+            }
+
+            Log.Debug($"{nameof(VerifySignature)} (exit)");
+            return isValid;
+        }
 
 
         //TODO: We may want to move the following serialize and deserialize methods into a serialization class? -Dsphar 4/6/2019
